e46008d23e5f51330e31e79fce0a725b593bd1e1
towlerj <jtowler@gmail.com>
2020-07-04 10:03:43 +0100

    and adding my own

---
+++ keyboards/ergodox_ez/keymaps/towlerj/config.h
@@ -0,0 +1,12 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 5
+
+#define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+#define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define DISABLE_RGB_MATRIX_RAINDROPS
