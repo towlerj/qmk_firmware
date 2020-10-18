c147297d22e85d6b6241489a5f12cc1cc54d93a0
towlerj <jtowler@gmail.com>
2020-10-18 11:48:28 +0100

    Initial alt setup

--- keyboards/massdrop/alt/keymaps/towlerj/rules.mk
+++ keyboards/massdrop/alt/keymaps/towlerj/rules.mk
@@ -1,3 +1,4 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-TAP_DANCE_ENABLE = yes
+OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
+#TAP_DANCE_ENABLE = yes
